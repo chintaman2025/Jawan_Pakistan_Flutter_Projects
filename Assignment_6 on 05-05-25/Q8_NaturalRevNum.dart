ef83431137994d8f267d31c57c92ca66b6f86e71
chintaman2025 <muhammadalich.1989@gmail.com>
2025-05-06 17:34:29 +0500

    Q8 the first 10 natural numbers in reverse order

---
+++ Assignment_6 on 05-05-25/Q8_NaturalRevNum.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  //Q8. Implement Dart code to print the first 10 natural numbers in reverse order using a while loop.
+  
+  
+  int a = 10;
+  print("List of Natural first 10 numbers in reverse :");
+
+  while (a > 0) 
+  {
+    print("$a");
+    a--;
+  }
+}
