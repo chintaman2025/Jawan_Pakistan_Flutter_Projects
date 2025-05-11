b82d42701c953bd0ad7e9a27142cf2cad10705ee
chintaman2025 <muhammadalich.1989@gmail.com>
2025-05-11 23:04:00 +0500

    Lecture on 11-05-25 Student Data in seprate file and inport in functions.dart file

---
+++ Lecture Functions 11-05-25/student_Data.dart
@@ -0,0 +1,22 @@
+/// This file contains the list of students with their class, subjects, and marks.
+
+List<Map<String, dynamic>> studentsData = [
+  {
+    "name": "Ali",
+    "class": "9th",
+    "subjects": ["English", "Urdu", "Physics", "Chemistry", "Biology"],
+    "marks": [33, 32, 54, 32, 54],
+  },
+  {
+    "name": "Owais",
+    "class": "9th",
+    "subjects": ["English", "Urdu", "Physics", "Chemistry", "Biology"],
+    "marks": [33, 43, 23, 32, 12],
+  },
+  {
+    "name": "Bilal",
+    "class": "9th",
+    "subjects": ["English", "Urdu", "Physics", "Chemistry", "Biology"],
+    "marks": [12, 76, 32, 98, 54],
+  },
+];
